ef2fa4d9d675029484d65a11370c1d61275df50c
HanCh <05.hanch.it@gmail.com>
2021-05-11 03:18:27 +0900

    firebase google 로그인 연동

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,14 +1,43 @@
-import { Avatar } from "@material-ui/core";
-
+import { Avatar, Button, IconButton } from "@material-ui/core";
 import styled from "styled-components";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+import * as EmailValidator from "email-validator";
+
 function Sidebar(props) {
+  const createChat = () => {
+    const input = prompt("Please enter an email address for the user you wish to chat with");
+
+    if(!input) return;
+
+    if(EmailValidator.validate(input)) {
+      // add the chat into the DB 'chats' collection
+      
+    }
+  };
   return (
     <Container>
       <Header>
         <UserAvatar />
-
-        <IconsContainer></IconsContainer>
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
       </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="채팅 검색" />
+      </Search>
+
+      <SidebarButton onClick={createChat}>새로운 채팅 만들기</SidebarButton>
+
+      {/* List of Chat */}
     </Container>
   );
 }
@@ -17,8 +46,47 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
 
-const UserAvatar = styled(Avatar)``;
+const SearchInput = styled.input`
+  outline: none;
+  border: none;
+  flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: #fff;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
 
 const IconsContainer = styled.div``;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import firebase from "firebase";
+import Login from "./login";
+import Loading from "./Loading";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users").doc(user.uid).set(
+        {
+          emai: user.email,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        {
+          marge: true,
+        }
+      );
+    }
+  }, [user]);
+  
+  if (loading) return <Loading />;
+  if (!user) return <Login />;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
