2315657b9ed72b5fc15f4e5defd60a5cbe65024f
HanCh <05.hanch.it@gmail.com>
2021-05-11 00:39:35 +0900

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,24 @@
+import { Avatar } from "@material-ui/core";
+
+import styled from "styled-components";
+function Sidebar(props) {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+
+        <IconsContainer></IconsContainer>
+      </Header>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
